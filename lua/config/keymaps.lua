b8a0c66aa10b2de7dd9c2f0ac1913e25a19f02fa
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-03-10 22:18:55 -0700

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+--
+-- O
 -- Add any additional keymaps here
+-- -- lua/config/keymaps.lua
+vim.keymap.set("n", "<leader>rn", function()
+  return ":IncRename " .. vim.fn.expand("<cword>")
+end, { expr = true })
+
+vim.keymap.set("n", "<leader>inc", function()
+  require("dial.map").manipulate("increment", "normal")
+end)
+vim.keymap.set("n", "<leader>dec", function()
+  require("dial.map").manipulate("decrement", "normal")
+end)
+vim.keymap.set("n", "g<leader>inc", function()
+  require("dial.map").manipulate("increment", "gnormal")
+end)
+vim.keymap.set("n", "g<leader>dec", function()
+  require("dial.map").manipulate("decrement", "gnormal")
+end)

ab8c9e3fb1cae15fcab6e17533f6d5c7ab78ee75
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-03-31 13:14:50 -0700

    added toggleterm stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,8 @@
 --
 -- O
 -- Add any additional keymaps here
+
+local Terminal = require("toggleterm.terminal").Terminal
 -- -- lua/config/keymaps.lua
 vim.keymap.set("n", "<leader>rn", function()
   return ":IncRename " .. vim.fn.expand("<cword>")
@@ -20,3 +22,31 @@ end)
 vim.keymap.set("n", "g<leader>dec", function()
   require("dial.map").manipulate("decrement", "gnormal")
 end)
+
+-- Persistent floating terminal instance
+local float_term = Terminal:new({
+  direction = "float",
+  hidden = true,
+  float_opts = {
+    border = "rounded",
+    width = 100,
+    height = 30,
+    winblend = 10,
+  },
+})
+
+-- <leader>ft → floating terminal in current working dir
+vim.keymap.set("n", "<leader>ft", function()
+  float_term:toggle()
+end, { desc = "Floating Terminal (cwd)" })
+
+-- <leader>fT → floating terminal in root dir
+vim.keymap.set("n", "<leader>fT", function()
+  float_term.dir = require("lazyvim.util").root()
+  float_term:toggle()
+end, { desc = "Floating Terminal (Root Dir)" })
+
+-- <C-\> → toggle same floating terminal
+vim.keymap.set({ "t" }, "<Esc>", function()
+  float_term:close()
+end, { desc = "Toggle Floating Terminal" })
